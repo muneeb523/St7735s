6e49e8b3058409f5a7c4a9515ee3e7d323e24d9a
aqib malik <aqibnasim@gmail.com>
2025-02-20 15:43:20 +0500

    testing pi

--- Application_code/Test.cpp
+++ Application_code/Test.cpp
@@ -2,83 +2,100 @@
 
 // Ensure C functions can be used in C++
 extern "C" {
-    #include "../st7735s.h"
-    #include "../fonts.h"
-    #include "../gfx.h"
-    #include "../image.h"
+#include "../st7735s.h"
+#include "../fonts.h"
+#include "../gfx.h"
+#include "../image.h"
 }
+
+// Define modes
+enum Mode {
+    CAMERA,
+    SOUND,
+    CALL,
+    TORCH
+};
+
+Mode current_mode = CAMERA;  // Start with Camera mode
 
 class DisplayExample {
 public:
     void run() {
         ST7735S_Init();
         setOrientation(R180);
-        
-        uint8_t f = 0;
+
         while (true) {
-            // f = 1 - f;
-            // if (f) {
-            //     std::cout << "Here at while loop" << std::endl;
-                setColor(0, 0, 0);
-                fillScreen();
-            // } else {
-            //     uint8_t r = 0, g = 0, b = 0;
-            //     for (int y = 0; y < HEIGHT; y++) {
-            //         for (int x = 0; x < WIDTH; x++) {
-            //             setColor(r, g, b);
-            //             setPixel(x, y);
-            //             r = x % 32;
-            //             g = (x + y) % 64;
-            //             b = y % 32;
-            //         }
-            //     }
-            // }
-
-            // for (int i = 0; i < 32; i++) {
-            //     setColor(i, i, i);
-            //     drawLine(0, i, WIDTH - 1, i);
-            // }
-
-            // setColor(31, 63, 31);
-            // setbgColor(0, 0, 0);
-            // setFont(ter_u24b);
-            // drawText(4, 33, "Hello World  !");
-
-            // for (int i = 0; i < 64; i++) {
-            //     setColor(i / 2, 63 - i, i / 2);
-            //     drawLine(WIDTH - 1 - i, 0, WIDTH - 1 - i, HEIGHT - 1);
-            // }
-            // for (int i = 0; i < 64; i++) {
-            //     setColor(i / 2, i, i / 2);
-            //     drawLine(0, HEIGHT - 1 - i, WIDTH - 1, HEIGHT - 1 - i);
-            // }
-
-            // setColor(10, 20, 30);
-            // drawCircle(80, 50, 30);
-
-            // setColor(30, 10, 30);
-            // drawCircle(40, 10, 90);
-
-            // setColor(30, 0, 30);
-            // drawLine(0, 0, WIDTH - 1, HEIGHT - 1);
-            drawImage(10, 10, cam_on, 28, 28);
-            drawImage(10, 70, cam_off, 28, 28);
-
-
-
-
-
-
-            flushBuffer();
+            drawUI();  // Refresh UI
+            waitForButtonPress(); // Wait for a button press to change mode
         }
+    }
+
+    void drawUI() {
+        fillScreen();  // Clear screen
+
+        // Define pie wedge angles
+        float angles[4][2] = {
+            {0, 90},    // CAMERA
+            {90, 180},  // SOUND
+            {180, 270}, // CALL
+            {270, 360}  // TORCH
+        };
+
+        // Draw pie wedges dynamically
+        for (int i = 0; i < 4; i++) {
+            if (i == current_mode) {
+                setColor(31, 31, 0);  // Active mode: Bright Yellow (RGB565)
+                drawPie(80, 40, 30, angles[i][0], angles[i][1]);  // Bigger wedge
+            } else {
+                setColor(10, 10, 10);  // Inactive mode: Dark gray
+                drawPie(80, 40, 20, angles[i][0], angles[i][1]);  // Smaller wedge
+            }
+        }
+
+        // Adjust icon positions to be inside the wedges
+        int iconX = 70, iconY = 30; // Default position
+
+        switch (current_mode) {
+            case CAMERA:
+                drawImage(iconX, iconY, cam_on, 28, 28);
+                break;
+            case SOUND:
+                drawImage(iconX, iconY, cam_off, 28, 28);
+                break;
+            case CALL:
+                drawImage(iconX, iconY, mic, 28, 28);
+                break;
+            case TORCH:
+                drawImage(iconX, iconY, cam_off, 28, 28);
+                break;
+        }
+
+        flushBuffer();  // Update display
+    }
+
+    void waitForButtonPress() {
+        while (!isButtonPressed()) {} // Wait until button press is detected
+        updateMode(); // Change mode when pressed
+    }
+
+    bool isButtonPressed() {
+        // Mock function - Replace with actual button read logic
+        static int counter = 0;
+        counter++;
+        return (counter % 2000000 == 0); // Simulate button press
+    }
+
+    // Change mode when button is pressed
+    void updateMode() {
+        current_mode = static_cast<Mode>((current_mode + 1) % 4);  // Cycle modes
+      //  drawUI();  // Refresh screen after mode change
     }
 };
 
+// Main function
 int main() {
-
     std::cout << "Start" << std::endl;
     DisplayExample display;
     display.run();
     return 0;
-
 }
