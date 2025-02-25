b6f7f99f3b903d975b2e5a5f7e3f9f1c93ec4dec
aqib malik <aqibnasim@gmail.com>
2025-02-25 15:04:44 +0500

    new

--- Application_code/Test.cpp
+++ Application_code/Test.cpp
@@ -11,16 +11,16 @@ extern "C"
 #include "../st7735s_compat.h"
 }
 
-enum Mode { CAMERA, SOUND, CALL, TORCH };
+enum Mode { STREAM, BUZZER, VoIP, FLASHLIGHT };
 
 struct ImageSize {
-    const uint16_t *image_24;
-    const uint16_t *image_32;
+    const uint16_t *image;
+    int width;
+    int height;
 };
 
 int i = 0;
-Mode current_mode = CAMERA;
-Mode next_mode = SOUND;
+Mode current_mode = STREAM;
 
 class DisplayExample {
 public:
@@ -36,35 +36,25 @@ public:
     }
 
     void drawUI() {
-        setColor(2, 2, 2);
+        setColor(0, 0, 0); // Black background
         fillScreen();
 
+        // Battery and Signal icons
         drawImage(5, 12, battery_good, 24, 24);
         drawImage(55, 15, signal, 20, 18);
 
-        struct IconPosition { int x, y; };
-        IconPosition positions[4] = {
-            {48, 60}, {10, 60}, {45, 95}, {10, 95}
+        // Modes with their respective image names & sizes
+        ImageSize modeImages[6] = {
+            {Mode1, 54, 60},
+            {Mode2, 60, 54},
+            {Mode3, 57, 57},
+            {Mode4, 57, 57},
+            {Mode5, 56, 57},
+            {Mode6, 57, 57}
         };
 
-        ImageSize imageSets[4] = {
-            {video_16_16, video_32_32},
-            {sound_16_16, sound_32_32},
-            {flash_16_16, flash_32_32},
-            {voip_16_16, voip_32_32}
-        };
-
-        // Max highlighting effect for selected modes
-        for (int j = 0; j < 4; j++) {
-            bool isSelected = (j == current_mode || j == next_mode);
-            int iconSize = isSelected ? 40 : 16; // Max pop-up size
-            int offset = isSelected ? -12 : 0; // More prominent pop-up effect
-
-            drawImage(positions[j].x, positions[j].y + offset,
-                      isSelected ? imageSets[j].image_32 : imageSets[j].image_24,
-                      iconSize, iconSize);
-        }
-
+        // Draw the complete mode image at a fixed position
+        drawImage(20, 50, modeImages[i].image, modeImages[i].width, modeImages[i].height);
         flushBuffer();
     }
 
@@ -77,26 +67,12 @@ public:
     bool isButtonPressed() {
         static int counter = 0;
         counter++;
-        return (counter % 500000 == 0); // Faster response to button press
-        _Delay(5000);
+        return (counter % 500000 == 0);
+        _Delay(10000);
     }
 
     void updateMode() {
-        if (i == 0) {
-            current_mode = CAMERA;
-            next_mode = SOUND;
-        } else if (i == 1) {
-            current_mode = SOUND;
-            next_mode = CALL;
-        } else if (i == 2) {
-            current_mode = CALL;
-            next_mode = TORCH;
-        } else if (i == 3) {
-            current_mode = TORCH;
-            next_mode = CAMERA;
-            i = -1;
-        }
-        i++;
+        i = (i + 1) % 6; // Cycle through 6 modes
     }
 };
 
