bb4c8e25cf8531b6890f6e0cc9745a330e649b4f
Muneeb <murrehman523@gmail.com>
2025-05-09 00:05:55 +0500

    new

--- appgpio.c
+++ appgpio.c
@@ -277,11 +277,11 @@ int areButtonsPressed(void)
         clock_gettime(CLOCK_MONOTONIC, &current_time);
         double elapsed = (current_time.tv_sec - start_time.tv_sec) +
                          (current_time.tv_nsec - start_time.tv_nsec) / 1e9;
-        // if (elapsed >= CHECK_TIMEOUT_S)
-        // {
-        //     close(fd);
-        //     return ERROR_TIMEOUT;
-        // }
+        if (elapsed >= CHECK_TIMEOUT_S)
+        {
+            close(fd);
+            return ERROR_TIMEOUT;
+        }
 
         // Check GPIO value
         enum gpiod_line_value gpio_val[1] ;//<Normal high state
